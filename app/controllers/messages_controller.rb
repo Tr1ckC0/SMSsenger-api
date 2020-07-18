087d2310d2e3cb0775e3d4be6e1c346657b8f436
Tr1ckC0 <trick.co@yahoo.com>
2020-07-18 15:20:58 -0500

    send / recieve actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,29 @@
 class MessagesController < ApplicationController
     include Plivo
-    CLIENT = RestClient.new(PLIVO_ID, PLIVO_TOKEN)
+    API = RestClient.new(PLIVO_ID, PLIVO_TOKEN)
 
     def index
         render json: "Hello from SMSsenger!"
     end
 
-    def send
-        message_created = CLIENT.messages.create(
+    # def create
+    #     binding.pry
+    #     I can set the URL in the send_sms action to hit this endpoint and create a message
+    # end
+
+    def send_sms
+        response = API.messages.create(
             '16282683456',
             %w[12106729886],
-            'Hello from Rails!'
+            'Send test 2!'
           )
+        puts response.message_uuid
     end
 
-    def receive
-
+    def receive_sms
+        from_number = params[:From]
+        to_number = params[:To]
+        text = params[:Text]
+        puts "Message received - From: #{from_number}, To: #{to_number}, Text: #{text}"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :messages, only: [:index]
+  get 'send', to: 'messages#send_sms'
+  get 'receive', to: 'messages#receive_sms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'messages#index'
 end
