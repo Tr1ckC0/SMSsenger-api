4204764c5c02c743726067880901bea325555a36
Tr1ckC0 <trick.co@yahoo.com>
2020-07-18 15:00:03 -0500

    send

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,20 @@
 class MessagesController < ApplicationController
-    
+    include Plivo
+    CLIENT = RestClient.new(PLIVO_ID, PLIVO_TOKEN)
+
     def index
-        render json: {message: "Hello World"}
+        render json: "Hello from SMSsenger!"
+    end
+
+    def send
+        message_created = CLIENT.messages.create(
+            '16282683456',
+            %w[12106729886],
+            'Hello from Rails!'
+          )
+    end
+
+    def receive
+
     end
 end
