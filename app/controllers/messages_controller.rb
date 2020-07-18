9d4854bdb666892f830db7322b09f21af7e7f7d4
Tr1ckC0 <trick.co@yahoo.com>
2020-07-18 17:42:40 -0500

    fixed cors issue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,36 +1,31 @@
 class MessagesController < ApplicationController
-    include Plivo
-    API = RestClient.new(PLIVO_ID, PLIVO_TOKEN)
+  include Plivo
+  API = RestClient.new(PLIVO_ID, PLIVO_TOKEN)
 
-    def index
-        render json: "Hello from SMSsenger!"
-    end
+  def index
+    render json: "Hello from SMSsenger!"
+  end
 
-    # def create
-    #     binding.pry
-    #     I can set the URL in the send_sms action to hit this endpoint and create a message
-    # end
+  def send_sms
+    dst, text = message_params.values_at(:To, :Text)
+    response = API.messages.create(
+      "16282683456",
+      [dst],
+      text
+    )
+    puts response.message_uuid
+  end
 
-    def send_sms
-        dst, text = message_params.values_at(:To, :Text)
-        response = API.messages.create(
-            '16282683456',
-            [dst],
-            text
-          )
-        puts response.message_uuid
-    end
+  def receive_sms
+    from_number = params[:From]
+    to_number = params[:To]
+    text = params[:Text]
+    puts "Message received - From: #{from_number}, To: #{to_number}, Text: #{text}"
+  end
 
-    def receive_sms
-        from_number = params[:From]
-        to_number = params[:To]
-        text = params[:Text]
-        puts "Message received - From: #{from_number}, To: #{to_number}, Text: #{text}"
-    end
+  private
 
-    private
-
-        def message_params
-            params.require(:message).permit(:To, :Text)
-        end
+  def message_params
+    params.require(:message).permit(:To, :Text)
+  end
 end
